a3c09eba2c4243f201ac4a80ec8946d6406afe9a
Byron Lee <byronn.lee@gmail.com>
2022-07-05 16:16:33 +0100

    first commit

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,26 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+/* 
+import localforage from "localforage";
+jest.mock("localforage");
+
+// eslint-disable-next-line no-global-assign
+console = {
+  ...console,
+  log: jest.fn(),
+  info: jest.fn(),
+  debug: jest.fn(),
+  error: jest.fn(),
+};
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  (localforage.getItem as jest.Mock).mockImplementation(() =>
+    Promise.resolve(null)
+  );
+  (localforage.setItem as jest.Mock).mockImplementation(() =>
+    Promise.resolve()
+  );
+});
+ */
