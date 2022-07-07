749e7b25549bfad31c6b1f48b64a09583621a005
Byron Lee <byronn.lee@gmail.com>
2022-07-07 20:34:31 +0100

    added tests and more to redux state object

--- src/data/store/gameSlice.test.ts
+++ src/data/store/gameSlice.test.ts
@@ -1,39 +1,93 @@
-import { store } from "./appStore";
+import { configureStore } from "@reduxjs/toolkit";
+import { GameStore, store } from "./appStore";
 import { takeMove } from "./gameSlice";
+import gameReducer from "./gameSlice";
 
 describe("game slice", () => {
-  it("has correct initial state", () => {
-    expect(store.getState()).toStrictEqual({
-      boardState: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      turn: 1,
+    it("has correct initial state", () => {
+        expect(store.getState()).toStrictEqual({
+            boardState: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            turn: 1,
+            winner: true,
+            winnerIs: 0,
     });
   });
-});
 
-describe("take move", () => {
-  it("success", () => {
-    store.dispatch(takeMove(1));
-    expect(store.getState()).toStrictEqual({
-      boardState: [0, 1, 0, 0, 0, 0, 0, 0, 0],
-      turn: 2,
+  describe("take move", () => {
+    let testStore: GameStore;
+    beforeEach(() => {
+      testStore = configureStore({
+        reducer: gameReducer,
+      });
     });
-    store.dispatch(takeMove(4));
-    expect(store.getState()).toStrictEqual({
-      boardState: [0, 1, 0, 0, 2, 0, 0, 0, 0],
-      turn: 1,
+
+    it("success", () => {
+      testStore.dispatch(takeMove(1));
+      expect(testStore.getState()).toStrictEqual({
+        boardState: [0, 1, 0, 0, 0, 0, 0, 0, 0],
+          turn: 2,
+          winner: true,
+          winnerIs: 0,
+      });
+      testStore.dispatch(takeMove(4));
+      expect(testStore.getState()).toStrictEqual({
+        boardState: [0, 1, 0, 0, 2, 0, 0, 0, 0],
+          turn: 1,
+          winner: true,
+          winnerIs: 0,
+      });
+      testStore.dispatch(takeMove(0));
+      expect(testStore.getState()).toStrictEqual({
+        boardState: [1, 1, 0, 0, 2, 0, 0, 0, 0],
+          turn: 2,
+          winner: true,
+          winnerIs: 0,
+      });
+      testStore.dispatch(takeMove(7));
+      expect(testStore.getState()).toStrictEqual({
+        boardState: [1, 1, 0, 0, 2, 0, 0, 2, 0],
+          turn: 1,
+          winner: true,
+          winnerIs: 0,
+      });
     });
-    store.dispatch(takeMove(0));
-    expect(store.getState()).toStrictEqual({
-      boardState: [1, 1, 0, 0, 2, 0, 0, 0, 0],
-      turn: 2,
+
+    it("placed counter out side boards allotted numbers", () => {
+      testStore = configureStore({
+        reducer: gameReducer,
+        preloadedState: {
+          boardState: [1, 1, 0, 0, 2, 0, 0, 2, 0],
+          turn: 1,
+        },
+      });
+      expect(() => {
+        testStore.dispatch(takeMove(12));
+      }).toThrow("counter placed outside board");
     });
-    store.dispatch(takeMove(7));
-    expect(store.getState()).toStrictEqual({
-      boardState: [1, 1, 0, 0, 2, 0, 0, 2, 0],
-      turn: 1,
+
+    it("place counter on top of counter", () => {
+      testStore = configureStore({
+        reducer: gameReducer,
+        preloadedState: {
+          boardState: [1, 1, 0, 0, 2, 0, 0, 2, 0],
+          turn: 1,
+        },
+      });
+      expect(() => {
+        testStore.dispatch(takeMove(7));
+      }).toThrow("space occupied");
     });
+
+      it("three in a row", () => {
+          testStore = configureStore({
+              reducer: gameReducer,
+              preloadedState: {
+                  boardState: [1, 1, 1, 0, 2, 0, 0, 2, 0],
+                  turn: 1,
+                  winner: true,
+                  winnerIs: 0,
+              },
+          });
+      });
   });
-  it("place counter out side board numbers", () => {});
-  it("place counter on top of counter", () => {});
-  it("three in a row", () => {});
 });
--- src/data/store/gameSlice.ts
+++ src/data/store/gameSlice.ts
@@ -2,10 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const gameSlice = createSlice({
   name: "game",
-  initialState: {boardState: [0, 0, 0, 0, 0, 0, 0, 0, 0], turn: 1},
+  initialState: {boardState: [0, 0, 0, 0, 0, 0, 0, 0, 0], turn: 1, winner:true, winnerIs:0},
   reducers: {
     takeMove(state, action) {
       const counterPosition = action.payload;
+
+      if (state.boardState[counterPosition] === 1 || state.boardState[counterPosition] === 2) {
+        throw new Error('space occupied');
+      }
+      if (counterPosition > state.boardState.length) {
+        throw new Error('counter placed outside board');
+      }
+
+      
       state.boardState[counterPosition] = state.turn;
       state.turn = state.turn === 2 ? 1 : 2;
     },
